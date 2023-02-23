c7b9c319f4c81965dd8142b510bddf22dc98bff9
Kijimai <jdbucog@yahoo.com>
2023-02-23 11:24:23 -0800

    init updateEntriesText function

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
+let maxPage = 1;
+let results = [];
+// let textSearchResults = []
+let foundData = [];
+
 async function getData() {
-  const data = await fetch("./data.json").then((res) => res.json())
-  createTableRow(data)
+  const data = await fetch("./data.json").then((res) => res.json());
+  results = data;
 }
 
-function createTableRow(data) {
-  const { id, first_name, last_name, email } = data
-  let rowHTML = `
-  <tr data-key=${id}>
-    <td>${first_name}</td>
-    <td>${last_name}</td>
-    <td>${email}</td>
-  </tr>
-`
-  return rowHTML
+function createTableRow(data, listHolder) {
+  const { first_name, last_name, email } = data;
+
+  const tableRowEl = document.createElement("tr");
+  const firstName = document.createElement("td");
+  const lastName = document.createElement("td");
+  const userEmail = document.createElement("td");
+  firstName.innerHTML = `${first_name}`;
+  lastName.innerHTML = `${last_name}`;
+  userEmail.innerHTML = `${email}`;
+  tableRowEl.appendChild(firstName);
+  tableRowEl.appendChild(lastName);
+  tableRowEl.appendChild(userEmail);
+  listHolder.push(tableRowEl);
 }
+
+function changePage(listHolder, currentPage, maxPage, input) {}
 
 function pagination(currentIndex, totalCount, maxEntrySize, list) {
-  const lastPage = Math.ceil(totalCount / maxEntrySize)
+  const lastPage = Math.ceil(totalCount / maxEntrySize);
 
   const newResults = Array.from({ length: lastPage }, (_, index) => {
-    const start = index * maxEntrySize
-    const end = start + maxEntrySize
+    const start = index * maxEntrySize;
+    const end = start + maxEntrySize;
 
-    return list.slice(start, end)
-  })
+    return list.slice(start, end);
+  });
 
-  const currentPageDisplayNum = currentIndex + 1
+  const currentPageDisplayNum = currentIndex + 1;
 
-  return {newResults, currentPageDisplayNum}
+  return { newResults, currentPageDisplayNum };
 }
 
-function drawTable() {}
+function drawTable(listItem, isFiltering = false) {
+  const tableBody = document.getElementById("table-body");
+  if (isFiltering) {
+    updateEntriesText();
+  }
+  tableBody.appendChild(listItem);
+}
 
-getData()
+/**
+ * Updates the text on showing entries display -- THESE VARIABLE NAMES MAY CHANGE
+ * @param {Number} currentEntryIndex The size of the current Index of results
+ * @param {Number} totalEntrySize Size of entries array
+ * @param {Number} maxEntrySize
+ * @param {Boolean} isFiltering Updates the showing entries text based on if the user is currently filtering exisiting results
+ */
+function updateEntriesText(
+  currentEntryIndex,
+  totalEntrySize,
+  maxEntrySize,
+  isFiltering
+) {
+  const entriesText = document.getElementById("entries-text");
+  let textValue = `Showing ${currentEntryIndex + 1} to ${
+    currentEntryIndex + 1 + maxEntrySize
+  } of ${totalEntrySize} entries`;
+
+  entriesText.innerHTML = textValue;
+}
+
+// getData()
